3a03d06d229a4966d923b77684006c344aed6ca0
keipertk <keipertk+github@gmail.com>
2018-05-01 14:58:46 -0500

    Update nwxJenkins.groovy

--- ci/Jenkins/nwxJenkins.groovy
+++ ci/Jenkins/nwxJenkins.groovy
@@ -60,7 +60,7 @@ def buildDependencies(String[] depends, cmakeCommand, credentialsID){
 
 for (int i=0; i<depends.size(); i++){
     dir("${depends[i]}"){
-        git credentialsId:"${credentialsID}"
+        git credentialsId:"${credentialsID}",
         url:"https://github.com/NWChemEx-Project/${depends[i]}.git",
         branch: 'master'
         compileRepo("${depends[i]}", "True", cmakeCommand, credentialsID)
