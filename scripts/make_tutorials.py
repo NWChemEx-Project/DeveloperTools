cc4664c35528e30d9326bbf2d8f647b3c69a67a6
ryanmrichard <ryanmrichard1@gmail.com>
2019-06-07 12:21:26 -0500

    tutorial script works for me

--- scripts/make_tutorials.py
+++ scripts/make_tutorials.py
@@ -29,7 +29,7 @@ def parse_file(cc, filename):
     tutor_skip_stop = cc + "TUTORIAL_STOP_SKIP"
 
     comments = []  # Will be the comments we find
-    code = [[]]  # Will be the code snippets we fine
+    code = []  # Will be the code snippets we fine
     in_comment = False  # True signals we are in a tutorial comment block
     skipping = False
     which_first = None
@@ -60,7 +60,7 @@ def parse_file(cc, filename):
                 comments.append([])  # Start a new comment block
                 in_comment = True
             elif is_comment and in_comment:  # Part of tutorial comment
-                comments[-1].append(no_ws[2:])  # Add line w/o comment character
+                comments[-1].append(no_ws[len(cc):])  # omit comment character
             elif in_comment:  # 1st line outside a comment block
                 in_comment = False
                 code.append([])
@@ -68,9 +68,36 @@ def parse_file(cc, filename):
             else:  # n-th line outside a comment block
                 if which_first is None:
                     which_first = False
+                if len(code) == 0: # If code came first, there's no array yet
+                    code.append([])
                 code[-1].append(line)
 
     return comments, code, which_first
+
+
+def strip_newline(block):
+    """
+    Strips any blank lines from the beginning and end of the block.
+    :param block: The block to parse.
+    :return: The block w/o the proceeding/trailing blank lines
+    """
+    start = 0
+    end = len(block)
+    for line in block:
+        if line.strip():
+            break
+        start += 1
+
+    # Early termination for empty block
+    if start == end:
+        return []
+
+    for line in reversed(block[start:]):
+        if line.strip():
+            break
+        end -= 1
+
+    return block[start: end]
 
 
 def write_code(block, lang):
@@ -84,27 +111,16 @@ def write_code(block, lang):
     :return: A string suitable for printing in a reST file
     """
     a_tab = " "*4
-    start = 0
-    end = len(block)
 
-    # Strip off any proceeding or trailing blank lines
-    for line in block:
-        if line.strip():
-            break
-        start += 1
+    parsed_block = strip_newline(block)
 
-    if start == end:  # Early termination for blank blocks
+    if len(parsed_block) == 0:
         return ""
-
-    for line in reversed(block):
-        if line.strip():
-            break
-        end -= 1
 
     # Assemble the actual code block
     output = ".. code:: {}\n\n".format(lang)
 
-    for line in block[start: end]:
+    for line in parsed_block:
         output += a_tab + line
     output += '\n'
 
@@ -112,39 +128,42 @@ def write_code(block, lang):
 
 
 def write_comment(block):
-    start = 0
-    end = len(block)
+    """
+    This function takes the raw tutorial comment block and turns it into a
+    string suitable for printing in a .rst file. This function does no
+    processing of the block aside from removing  proceeding and trailing
+    new lines
 
-    # Strip off any proceeding or trailing blank lines
-    for line in block:
-        if line.strip():
-            break
-        start += 1
-
-    if start == end:  # Early termination for blank blocks
-        return ""
-
-    for line in reversed(block):
-        if line.strip():
-            break
-        end -= 1
+    :param block: The tutorial block comment to print out.
+    :return: The comment-blcok reST-itized.
+    """
 
     output = ""
-    for line in block[start : end]:
+    for line in strip_newline(block):
         output += line
     output += '\n'
     return output
 
 
-def write_tutorial(name, first, comments, code, lang):
+def write_tutorial(name, lang, comments, code, first):
     """
     Given the parsed comments and code blocks this function will write out the
-    full reST page for the tutorial.
+    full reST page for the tutorial. It assumes that the code blocks and
+    comment blocks are interspersed, i.e. the original file went
+
+    code, comment, code, ...
+
+    or
+
+    comment, code, comment, ...
+
+    Consequentially the number of code and comment blocks must be equal or can
+    differ by at most one.
 
     :param name: The name of the tutorial. Will be used for the title.
-    :param frist: True if a comment was first and false if a code block was
     :param comments: A list of the comment blocks.
     :param code: A list of the code blocks.
+    :param frist: True if a comment was first and false if a code block was
     :param lang: The language for the reST code snippets
     :return:
     """
@@ -154,87 +173,96 @@ def write_tutorial(name, first, comments, code, lang):
     n_first = len(first_list)
     n_second = len(second_list)
 
+    # The first list needs to be either the same size as the second or one more
+    if n_first - n_second > 1:
+        raise Exception("abs(#code blocks - #comment blocks) > 1")
+
     # Write the title of the tutorial
     output = name + '\n'
-    output += '='*(len(name) - 1) + "\n\n"
+    output += '='*len(name) + "\n\n"
 
-    for i in range(n_first):
+    for i in range(n_second):  # Loop over second because it is possibly shorter
         if first:
             output += write_comment(comments[i])
-        else:
-            write_code(code[i], lang)
-        if i >= n_second:
-            continue
-        if first:
             output += write_code(code[i], lang)
         else:
+            output += write_code(code[i], lang)
             output += write_comment(comments[i])
+
+    if n_first == n_second + 1:
+        if first:
+            output += write_comment(comments[-1])
+        else:
+            output += write_code(code[-1], lang)
 
     return output
 
-# def make_tutorials(input, output) :
-#     """
-#     Given a full path to a directory, this function will parse each .py and
-#     .hpp file it finds. This process is repeated recursively for each
-#     subdirectory.
-#
-#     For a given C++ file this script looks for blocks like:
-#
-#     //TUTORIAL
-#     //
-#     //
-#     // Some documentation for the tutorial
-#     //
-#
-#     and Python blocks like:
-#
-#     #TUTORIAL
-#     #
-#     #
-#     # Some documentation for the tutorial
-#     #
-#
-#     :param dir: The full path to the directory containing the tutorials.
-#     :return:
-#     """
-#
-#     #Define the comment characters for each language
-#     py_comm = '#'
-#     c_comm  = "//"
-#
-#
-#     #Make output directory if it does not exist
-#     if not os.path.exists(output):
-#         os.mkdir(output)
-#
-#     filenames = []
-#
-#     for filename in os.listdir(input):
-#         if not filename.endswith(".hpp"):
-#             continue
-#         infile = os.path.join(input, filename)
-#         filenames.append(filename.split(".")[0])
-#         outfile = os.path.join(output, filenames[-1] + ".rst")
-#         print(outfile)
-#         comments, code = parse_file(infile)
-#         with open(outfile, 'w') as output_file:
-#             good_name = filenames[-1].replace('_', ' ').title()
-#             tutorial_name = good_name + " Tutorial\n"
-#             output_file.write(tutorial_name)
-#             output_file.write('='*(len(tutorial_name) - 1) + "\n\n")
-#             write_tutorial(output_file, comments, code)
-#
-#     with open(os.path.join(output, "index.rst"), 'w') as index_file:
-#         index_file.write("List of Tutorials\n")
-#         index_file.write("=================\n\n")
-#         index_file.write(".. toctree::\n")
-#         index_file.write("    :maxdepth: 2\n")
-#         index_file.write("    :caption: Contents:\n\n")
-#         for file in filenames:
-#             index_file.write("    " + file +"\n")
-#
-#
-#
+
+def write_index(file_names):
+    output = "List of Tutorials\n"\
+             "=================\n\n"\
+             ".. toctree::\n"\
+             "    :maxdepth: 2\n"\
+             "    :caption: Contents:\n\n"
+    for file_i in file_names:
+        output += "    " + file_i + "\n"
+    return output
+
+
+def make_tutorials(input_dir, output_dir):
+    """
+    Given a full path to a directory, this function will parse each source file
+    it finds. This process is repeated recursively for each subdirectory. For
+    each tutorial file, a .rst file with the same name will be created which
+    contains the contents of the source file converted to reST. The resulting
+    tutorial will be named X, where X is the file name (without extension)
+    converted to title case with underscores replace by spaces.
+
+    :param input_dir: The full path to the directory containing the tutorials.
+    :param output_dir: The full path to the directory where the tutorials should
+                       go
+    :return: nothing
+    """
+
+    # Define the comment characters and name for each language
+    comment_chars = {".py": '#', ".hpp": "//", ".cmake": '#'}
+    lang = {".py": "python", ".hpp": "c++", ".cmake": "cmake"}
+
+    # Make output directory if it does not exist
+    if not os.path.exists(output_dir):
+        os.mkdir(output_dir)
+
+    file_names = []  # Will be list of reST files we make
+
+    for file_name in os.listdir(input_dir):
+        input_file = os.path.join(input_dir, file_name)
+        file_name_base, extension = os.path.splitext(file_name)
+
+        if os.path.isdir(input_file):
+            make_tutorials(input_file, os.path.join(output_dir, file_name))
+            file_names.append(os.path.join(file_name,"index"))
+            continue
+
+        if extension not in lang:  # Not a file we're supposed to parse
+            continue
+
+        # Assemble the name of input file, output file, and tutorial
+
+        output_file = os.path.join(output_dir, file_name_base + ".rst")
+        tutorial_name = file_name_base.replace('_', ' ').title()
+
+        file_names.append(file_name_base)
+
+        rv = write_tutorial(tutorial_name, lang[extension],
+                            *parse_file(comment_chars[extension], input_file))
+        with open(output_file, 'w') as output_file:
+            output_file.write(rv)
+
+    with open(os.path.join(output_dir, "index.rst"), 'w') as index_file:
+        index_file.write(write_index(file_names))
+
+
+
 # def main():
 #     docs_dir = os.path.dirname(os.path.realpath(__file__))
 #     root_dir = os.path.dirname(docs_dir)
--- tests/test_make_tutorials.py
+++ tests/test_make_tutorials.py
@@ -1,9 +1,11 @@
 import os
 import sys
 import unittest
+import shutil
 
 # Get the path to the make_tutorials.py script and import it
 current_dir = os.path.dirname(os.path.realpath(__file__))
+src_dir = os.path.join(current_dir, "make_tutorials", "src")
 dt_root_dir = os.path.dirname(current_dir)
 scripts_dir = os.path.join(dt_root_dir, "scripts")
 sys.path.append(scripts_dir)
@@ -13,11 +15,11 @@ from make_tutorials import *
 
 class TestParseCxxFile(unittest.TestCase):
     def setUp(self):
-        self.path = os.path.join(current_dir, "make_tutorials", "cxx.hpp")
+        self.path = os.path.join(src_dir, "cxx.hpp")
         self.corr_code = [['\n', 'namespace test {\n', '\n'],
                           ['int function() { return 2; }\n', '\n',
                            '} //End namespace\n']]
-        self.corr_comment = [['\n', ' A line to include\n']]
+        self.corr_comment = [['\n', 'A line to include\n']]
 
     def test_parse(self):
         comments, code, comment_first = parse_file("//", self.path)
@@ -28,7 +30,7 @@ class TestParseCxxFile(unittest.TestCase):
 
 class TestWriteCxxCode(unittest.TestCase):
     def setUp(self):
-        self.path = os.path.join(current_dir, "make_tutorials", "cxx.hpp")
+        self.path = os.path.join(src_dir, "cxx.hpp")
         self.comments, self.code, self.first = parse_file("//", self.path)
 
     def test_block0(self):
@@ -45,55 +47,109 @@ class TestWriteCxxCode(unittest.TestCase):
 
 class TestWriteCxxComment(unittest.TestCase):
     def setUp(self):
-        self.path = os.path.join(current_dir, "make_tutorials", "cxx.hpp")
+        self.path = os.path.join(src_dir, "cxx.hpp")
         self.comments, self.code, self.first = parse_file("//", self.path)
 
     def test_block0(self):
         result = write_comment(self.comments[0])
-        corr = " A line to include\n\n"
+        corr = "A line to include\n\n"
         self.assertEqual(corr, result)
 
 
+class TestWriteCxxTutorial(unittest.TestCase):
+    def setUp(self):
+        self.path = os.path.join(src_dir, "cxx.hpp")
+        self.comments, self.code, self.first = parse_file("//", self.path)
 
-# class TestMakeCxxTutorial(unittest.TestCase):
-#     def setUp(self):
-#         self.path = os.path.join(current_dir, "make_tutorials", "cxx.hpp")
-#         self.comments, self.code, self.first = parse_file("//", self.path)
-#
-#     def test_write(self):
-#         rv = write_tutorial("Cxx Parsing Test", self.first, self.comments,
-#                             self.code, "C++")
-#         print(rv)
-#
-#
-# class TestParsePyFile(unittest.TestCase):
-#     def setUp(self):
-#         self.path = os.path.join(current_dir, "make_tutorials", "py.py")
-#         self.corr_code =[['\n\ndef function():\n', '    return 2\n']]
-#         self.corr_comment = [['', 'A line to include\n']]
-#
-#     def test_parse(self):
-#         comments, code, comment_first = parse_file("#", self.path)
-#         self.assertEqual(comments, self.corr_comment)
-#         self.assertEqual(code, self.corr_code)
-#         self.assertTrue(comment_first)
+    def test_tutorial(self):
+        result = write_tutorial("cxx tutorial",
+                                "C++",
+                                self.comments,
+                                self.code,
+                                self.first)
+        corr = "cxx tutorial\n"\
+               "============\n\n"\
+               ".. code:: C++\n\n"\
+               "    namespace test {\n\n"\
+               "A line to include\n\n"\
+               ".. code:: C++\n\n"\
+               "    int function() { return 2; }\n"\
+               "    \n"\
+               "    } //End namespace\n\n"
+        self.assertEqual(corr, result)
 
 
-# class TestWritePyFile(unittest.TestCase):
-#     def setUp(self):
-#         self.path = os.path.join(current_dir, "make_tutorials", "py.py")
-#         self.comments, self.code = parse_file("#", self.path)
-#
-#     def test_block0(self):
-#         result = write_code(self.code[0], "py")
-#         corr = ""
-#         self.assertEqual(corr, result)
-#
-#     def test_block1(self):
-#         result = write_code(self.code[1], "py")
-#         corr = ".. code:: py\n\n    def function():\n        return 2\n\n"
-#         self.assertEqual(corr, result)
-#
+class TestParsePyFile(unittest.TestCase):
+    def setUp(self):
+        self.path = os.path.join(src_dir, "py.py")
+        self.corr_code =[['\n', '\n', "def function():\n", "    return 2\n"]]
+        self.corr_comment = [['\n', 'A line to include\n']]
+
+    def test_parse(self):
+        comments, code, comment_first = parse_file('#', self.path)
+        self.assertEqual(comments, self.corr_comment)
+        self.assertEqual(code, self.corr_code)
+        self.assertTrue(comment_first)
+
+
+class TestWritePyCode(unittest.TestCase):
+    def setUp(self):
+        self.path = os.path.join(src_dir, "py.py")
+        self.comments, self.code, self.first = parse_file("#", self.path)
+
+    def test_block0(self):
+        result = write_code(self.code[0], "py")
+        corr = ".. code:: py\n\n    def function():\n        return 2\n\n"
+        self.assertEqual(corr, result)
+
+
+class TestWritePyComment(unittest.TestCase):
+    def setUp(self):
+        self.path = os.path.join(src_dir, "py.py")
+        self.comment, self.code, self.first = parse_file('#', self.path)
+
+    def test_write0(self):
+        result = write_comment(self.comment[0])
+        corr = "A line to include\n\n"
+        self.assertEqual(corr, result)
+
+
+class TestWritePyTutorial(unittest.TestCase):
+    def setUp(self):
+        self.path = os.path.join(src_dir, "py.py")
+        self.comment, self.code, self.first = parse_file('#', self.path)
+
+    def test_write(self):
+        result = write_tutorial("python tutorial",
+                                "py",
+                                self.comment,
+                                self.code,
+                                self.first)
+        corr = "python tutorial\n"\
+               "===============\n\n"\
+               "A line to include\n\n"\
+               ".. code:: py\n\n"\
+               "    def function():\n"\
+               "        return 2\n\n"
+
+        self.assertEqual(corr, result)
+
+class TestMakeTutorials(unittest.TestCase):
+    def setUp(self):
+        self.input_path = src_dir
+        self.output_path = os.path.join(current_dir, "make_tutorials", "test")
+
+    def test_make_tutorials(self):
+        make_tutorials(self.input_path, self.output_path)
+
+        for x in ["cxx.rst", "index.rst", "py.rst"]:
+            rst_file = os.path.join(self.output_path, x)
+            self.assertTrue(os.path.exists(rst_file))
+
+    def tearDown(self):
+        if os.path.isdir(self.output_path):
+            shutil.rmtree(self.output_path)
+
 
 if __name__ == "__main__":
     unittest.main(verbosity=2)
